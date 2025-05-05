fdd3e6815508c91c4bc087a0bf54bc00e6c77529
Dimas Cahyo Margono <136548518+dimzkuy@users.noreply.github.com>
2025-05-06 00:40:13 +0700

    Menghapus file yang tidak diperlukan

--- Movie_Booking/modules/report/pdf_exporter.py
+++
@@ -1,9 +0,0 @@
-from modules.report.exporter import ReportExporter
-
-class PDFExporter(ReportExporter):
-    def write_data(self, data, filename):
-        with open(filename, 'w') as f:
-            f.write("PDF EXPORT SIMULASI\n\n")
-            for row in data:
-                f.write("\n".join(f"{k}: {v}" for k, v in row.items()))
-                f.write("\n---\n")
